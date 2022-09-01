9a1aa283fa8320ec4f6106f40f59110370d424b9
Darksied95 <aliyuefe9501@gmail.com>
2022-09-01 21:49:06 +0100

    added functionalities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./assets/card-logo.svg";
+import done from "./assets/icon-complete.svg";
 import "./App.css";
 
 const App = () => {
+  const [user, setUser] = useState({
+    cardName: "",
+    cardNumber: "",
+    month: "",
+    year: "",
+    cvc: "",
+  });
+
+  const handleUserChange = ({ target }) => {
+    let newUser = { ...user };
+    newUser[target.name] = target.value;
+    setUser(newUser);
+  };
+  console.log(user);
   return (
-    <div>
+    <div className="body">
       <section className="first-section">
         <div className="back-card">
           <p>000</p>
@@ -22,27 +37,65 @@ const App = () => {
         <form>
           <div>
             <label htmlFor="name">CARDHOLDER NAME</label>
-            <input id="name" placeholder="e.g. Jane Appleseed" />
+            <input
+              id="name"
+              placeholder="e.g. Jane Appleseed"
+              name="cardName"
+              value={user.name}
+              onChange={handleUserChange}
+            />
           </div>
           <div>
             <label htmlFor="number">CARD NUMBER</label>
-            <input id="number" placeholder="e.g. 1234 5678 9123 0000" />
+            <input
+              id="number"
+              placeholder="e.g. 1234 5678 9123 0000"
+              name="cardNumber"
+              value={user.number}
+              onChange={handleUserChange}
+            />
           </div>
           <div className="other-card-details">
             <div className="expiry-date">
               <label htmlFor="expiry-date">EXP. DATE (MM/YY)</label>
               <div>
-                <input id="expiry-date" placeholder="MM" />
-                <input placeholder="YY" />
+                <input
+                  id="expiry-date"
+                  placeholder="MM"
+                  max="12"
+                  maxLength="2"
+                  name="month"
+                  value={user.month}
+                  onChange={handleUserChange}
+                />
+                <input
+                  placeholder="YY"
+                  name="year"
+                  maxLength="4"
+                  value={user.year}
+                  onChange={handleUserChange}
+                />
               </div>
             </div>
             <div className="cvc">
               <label htmlFor="cvc">CVC</label>
-              <input id="cvc" placeholder="e.g. 123" />
+              <input
+                id="cvc"
+                placeholder="e.g. 123"
+                name="cvc"
+                value={user.cvc}
+                onChange={handleUserChange}
+              />
             </div>
           </div>
           <button>Confirm</button>
         </form>
+        <div className="done">
+          <img src={done} alt="done-logo" />
+          <h1>THANK YOU!</h1>
+          <p>We've added your card details</p>
+          <button>Continue</button>
+        </div>
       </section>
     </div>
   );
